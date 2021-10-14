8d6f54859a56775c6f9d0aeac6f11589481cb0e0
jorgeayalah <69476275+jorgeayalah@users.noreply.github.com>
2021-10-14 09:31:21 -0500

    Initial Commit

---
+++ App07-ImagePuzzle/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App07-ImagePuzzle
+//
+//  Created by Alumno on 14/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
